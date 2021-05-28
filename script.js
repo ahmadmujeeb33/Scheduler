1f52eb474456216ef714bae836fcfb0115661f86
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-05-28 17:34:31 -0400

    day updated

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let body = $("body");
 let times = $(".times");
 
+let today = moment();
+
+$("#currentDay").text(today.format("dddd MMM Do"));
+
 // times.addClass('row hour');
 
 // times.addClass('hour textarea saveBtn');
